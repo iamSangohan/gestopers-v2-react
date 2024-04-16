bd2d23c3b8f8e4dde49e09932d51fde5ddfde027
Sangohan <89817683+iamSangohan@users.noreply.github.com>
2024-04-16 20:40:36 +0000

    #feat : Index page

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
+
+function Header() {
+    return (
+        <>
+            <header id="entete">
+                <nav class="navbar navbar-expand-lg bg-warning h-100">
+                    <div class="container-fluid px-5">
+                        <a class="navbar-brand fs-2 fw-semibold px-2" href="#">GESTOPERS</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item px-2">
+                                    <a class="nav-link fs-5 fw-medium" href="#">Tableau de bord</a>
+                                </li>
+                                <li class="nav-item px-2">
+                                    <a class="nav-link fs-5 fw-medium" href="#">Departements</a>
+                                </li>
+                                <li class="nav-item px-2">
+                                    <a class="nav-link fs-5 fw-medium" href="#">Permissions</a>
+                                </li>
+                                <li class="nav-item px-2">
+                                    <a class="nav-link fs-5 fw-medium" href="#">
+                                        <FontAwesomeIcon icon={faRightFromBracket} />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
