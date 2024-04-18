5454dd26c31b9f8aef79cd4083c5eb447024f6a6
Sangohan <89817683+iamSangohan@users.noreply.github.com>
2024-04-18 19:03:47 +0000

    #feat : manage form data et create logic to change a page after a from

--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from '../components/Header'
 
 import "./../styles/index.css"
+import { useNavigate } from 'react-router-dom'
 
 
 function Connexion() {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        // Vérifier les identifiants
+        if (username !== 'admin' || password !== 'admin') {
+            alert('Nom d\'utilisateur ou mot de passe incorrect')
+            return
+        } else {
+            // Stocker les identifiants dans le localStorage
+            localStorage.setItem('isConnected', true)
+
+            // Diriger vers la page Dashboard
+            navigate('/dashboard', { replace: true })
+        }
+    }
+
     return (
         <>
             <Header />
@@ -17,16 +38,20 @@ function Connexion() {
                     <div className="col-md-6">
                         <div className="card w-100">
                             <div className="card-body">
-                                <form>
+                                <form onSubmit={handleSubmit}>
                                     <div className="mb-4">
                                         <label className="form-label">Nom d'utilisateur</label>
-                                        <input type="text" className="form-control" />
+                                        <input  type="text" className="form-control" 
+                                                value={username} 
+                                                onChange={(e) => setUsername(e.target.value)} />
                                     </div>
                                     <div className="mb-4">
-                                        <label className="form-label">Mots de passe</label>
-                                        <input type="password" className="form-control" />
+                                        <label className="form-label">Mot de passe</label>
+                                        <input type="password" className="form-control"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)} />
                                     </div>
-                                    <button className="btn btn-primary w-100 py-8 mb-4 rounded-2">Lancer l'analyse</button>
+                                    <button className="btn btn-primary w-100 py-8 mb-4 rounded-2" type='submit'>Lancer l'analyse</button>
                                 </form>
                             </div>
                         </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import Header from '../components/Header'
 
 function Dashboard() {
+
+    // Verifier si l'utilisateur est connecté
+    if (!localStorage.getItem('isConnected')) {
+        // Send message d'erreur 
+        alert('Vous devez vous connecter pour accéder à cette page')
+        window.location.href = '/login'
+    }
+
     return (
         <>
             <Header />
